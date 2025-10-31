cb6dc5a80c75ed601552105d1f1a2598760c96fa
MonsterCry <49024516+monsterCry@users.noreply.github.com>
2025-11-01 01:13:20 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+    build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './intr.html',
+      }
+    }
+  },
   server: {
     cors: true,
     proxy: {
